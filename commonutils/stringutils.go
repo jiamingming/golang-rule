8fcc5302d82da5d5300a92b6db08dd6a972d40c2
jiamm <jiamingm@outlook.com>
2020-08-03 14:15:20 +0800

    init--

--- commonutils/stringutils.go
+++ commonutils/stringutils.go
@@ -1,7 +1,14 @@
 package commonutils
 
-import "bytes"
+import (
+	"bytes"
+	"unsafe"
+)
 
+/*
+* 拼接字符串
+*
+ */
 func ContactString(item ...string) string {
 
 	var item_buffer bytes.Buffer
@@ -9,4 +16,20 @@ func ContactString(item ...string) string {
 		item_buffer.WriteString(v)
 	}
 	return item_buffer.String()
+}
+
+/*
+* string转换为[]byte
+ */
+func Str2bytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+/*
+* []byte转换为string
+ */
+func Bytes2str(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
 }
